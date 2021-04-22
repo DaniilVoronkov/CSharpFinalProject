83e670e077b15b3c048a91692e1be2e68a60b96a
FloraKolikyan <79173232+FloraKolikyan@users.noreply.github.com>
2021-04-22 17:20:46 -0400

    Add files via upload

---
+++ CreateForm.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace FinalProject
+{
+    partial class CreateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstNameTextbox = new System.Windows.Forms.TextBox();
+            this.LastNameTextbox = new System.Windows.Forms.TextBox();
+            this.CountryTextbox = new System.Windows.Forms.TextBox();
+            this.AgeTextbox = new System.Windows.Forms.TextBox();
+            this.AssistsTextbox = new System.Windows.Forms.TextBox();
+            this.GoalsTextbox = new System.Windows.Forms.TextBox();
+            this.TeamTextbox = new System.Windows.Forms.TextBox();
+            this.NumberTextbox = new System.Windows.Forms.TextBox();
+            this.MatchesAmountTextbox = new System.Windows.Forms.TextBox();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.LastNameLabel = new System.Windows.Forms.Label();
+            this.FirstNameLabel = new System.Windows.Forms.Label();
+            this.PlayersAgeLabel = new System.Windows.Forms.Label();
+            this.PlayersCountryLabel = new System.Windows.Forms.Label();
+            this.PlayersAssistsLabel = new System.Windows.Forms.Label();
+            this.PlayersGoalsLabel = new System.Windows.Forms.Label();
+            this.PlayersNumberLabel = new System.Windows.Forms.Label();
+            this.PlayersTeamLabel = new System.Windows.Forms.Label();
+            this.PlayersMatchesAmountLabel = new System.Windows.Forms.Label();
+            this.PositionOnFieldTextBox = new System.Windows.Forms.TextBox();
+            this.PlayersPositionLabel = new System.Windows.Forms.Label();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstNameTextbox
+            // 
+            this.FirstNameTextbox.Location = new System.Drawing.Point(123, 112);
+            this.FirstNameTextbox.Name = "FirstNameTextbox";
+            this.FirstNameTextbox.Size = new System.Drawing.Size(146, 20);
+            this.FirstNameTextbox.TabIndex = 0;
+            // 
+            // LastNameTextbox
+            // 
+            this.LastNameTextbox.Location = new System.Drawing.Point(513, 112);
+            this.LastNameTextbox.Name = "LastNameTextbox";
+            this.LastNameTextbox.Size = new System.Drawing.Size(146, 20);
+            this.LastNameTextbox.TabIndex = 1;
+            this.LastNameTextbox.UseWaitCursor = true;
+            // 
+            // CountryTextbox
+            // 
+            this.CountryTextbox.Location = new System.Drawing.Point(123, 167);
+            this.CountryTextbox.Name = "CountryTextbox";
+            this.CountryTextbox.Size = new System.Drawing.Size(146, 20);
+            this.CountryTextbox.TabIndex = 2;
+            // 
+            // AgeTextbox
+            // 
+            this.AgeTextbox.Location = new System.Drawing.Point(513, 167);
+            this.AgeTextbox.Name = "AgeTextbox";
+            this.AgeTextbox.Size = new System.Drawing.Size(146, 20);
+            this.AgeTextbox.TabIndex = 3;
+            // 
+            // AssistsTextbox
+            // 
+            this.AssistsTextbox.Location = new System.Drawing.Point(513, 230);
+            this.AssistsTextbox.Name = "AssistsTextbox";
+            this.AssistsTextbox.Size = new System.Drawing.Size(146, 20);
+            this.AssistsTextbox.TabIndex = 4;
+            // 
+            // GoalsTextbox
+            // 
+            this.GoalsTextbox.Location = new System.Drawing.Point(123, 230);
+            this.GoalsTextbox.Name = "GoalsTextbox";
+            this.GoalsTextbox.Size = new System.Drawing.Size(146, 20);
+            this.GoalsTextbox.TabIndex = 5;
+           // this.GoalsTextbox.TextChanged += new System.EventHandler(this.GoalsTextbox_TextChanged);
+            // 
+            // TeamTextbox
+            // 
+            this.TeamTextbox.Location = new System.Drawing.Point(513, 298);
+            this.TeamTextbox.Name = "TeamTextbox";
+            this.TeamTextbox.Size = new System.Drawing.Size(146, 20);
+            this.TeamTextbox.TabIndex = 6;
+            // 
+            // NumberTextbox
+            // 
+            this.NumberTextbox.Location = new System.Drawing.Point(123, 298);
+            this.NumberTextbox.Name = "NumberTextbox";
+            this.NumberTextbox.Size = new System.Drawing.Size(146, 20);
+            this.NumberTextbox.TabIndex = 7;
+            // 
+            // MatchesAmountTextbox
+            // 
+            this.MatchesAmountTextbox.Location = new System.Drawing.Point(123, 363);
+            this.MatchesAmountTextbox.Name = "MatchesAmountTextbox";
+            this.MatchesAmountTextbox.Size = new System.Drawing.Size(146, 20);
+            this.MatchesAmountTextbox.TabIndex = 9;
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.BackColor = System.Drawing.Color.Transparent;
+            this.MainLabel.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainLabel.Location = new System.Drawing.Point(304, 33);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(175, 27);
+            this.MainLabel.TabIndex = 10;
+            this.MainLabel.Text = "Edit the info:";
+            // 
+            // LastNameLabel
+            // 
+            this.LastNameLabel.AutoSize = true;
+            this.LastNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.LastNameLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastNameLabel.Location = new System.Drawing.Point(388, 112);
+            this.LastNameLabel.Name = "LastNameLabel";
+            this.LastNameLabel.Size = new System.Drawing.Size(91, 17);
+            this.LastNameLabel.TabIndex = 11;
+            this.LastNameLabel.Text = "Last name:";
+            // 
+            // FirstNameLabel
+            // 
+            this.FirstNameLabel.AutoSize = true;
+            this.FirstNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.FirstNameLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstNameLabel.Location = new System.Drawing.Point(12, 112);
+            this.FirstNameLabel.Name = "FirstNameLabel";
+            this.FirstNameLabel.Size = new System.Drawing.Size(98, 17);
+            this.FirstNameLabel.TabIndex = 12;
+            this.FirstNameLabel.Text = "First name:";
+            // 
+            // PlayersAgeLabel
+            // 
+            this.PlayersAgeLabel.AutoSize = true;
+            this.PlayersAgeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayersAgeLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayersAgeLabel.Location = new System.Drawing.Point(437, 167);
+            this.PlayersAgeLabel.Name = "PlayersAgeLabel";
+            this.PlayersAgeLabel.Size = new System.Drawing.Size(40, 17);
+            this.PlayersAgeLabel.TabIndex = 13;
+            this.PlayersAgeLabel.Text = "Age:";
+            // 
+            // PlayersCountryLabel
+            // 
+            this.PlayersCountryLabel.AutoSize = true;
+            this.PlayersCountryLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayersCountryLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayersCountryLabel.Location = new System.Drawing.Point(26, 167);
+            this.PlayersCountryLabel.Name = "PlayersCountryLabel";
+            this.PlayersCountryLabel.Size = new System.Drawing.Size(84, 17);
+            this.PlayersCountryLabel.TabIndex = 14;
+            this.PlayersCountryLabel.Text = "Country:";
+            // 
+            // PlayersAssistsLabel
+            // 
+            this.PlayersAssistsLabel.AutoSize = true;
+            this.PlayersAssistsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayersAssistsLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayersAssistsLabel.Location = new System.Drawing.Point(409, 230);
+            this.PlayersAssistsLabel.Name = "PlayersAssistsLabel";
+            this.PlayersAssistsLabel.Size = new System.Drawing.Size(70, 17);
+            this.PlayersAssistsLabel.TabIndex = 15;
+            this.PlayersAssistsLabel.Text = "Assists:";
+            // 
+            // PlayersGoalsLabel
+            // 
+            this.PlayersGoalsLabel.AutoSize = true;
+            this.PlayersGoalsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayersGoalsLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayersGoalsLabel.Location = new System.Drawing.Point(47, 233);
+            this.PlayersGoalsLabel.Name = "PlayersGoalsLabel";
+            this.PlayersGoalsLabel.Size = new System.Drawing.Size(59, 17);
+            this.PlayersGoalsLabel.TabIndex = 16;
+            this.PlayersGoalsLabel.Text = "Goals:";
+            // 
+            // PlayersNumberLabel
+            // 
+            this.PlayersNumberLabel.AutoSize = true;
+            this.PlayersNumberLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayersNumberLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayersNumberLabel.Location = new System.Drawing.Point(32, 298);
+            this.PlayersNumberLabel.Name = "PlayersNumberLabel";
+            this.PlayersNumberLabel.Size = new System.Drawing.Size(74, 17);
+            this.PlayersNumberLabel.TabIndex = 17;
+            this.PlayersNumberLabel.Text = "Number:";
+            // 
+            // PlayersTeamLabel
+            // 
+            this.PlayersTeamLabel.AutoSize = true;
+            this.PlayersTeamLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayersTeamLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayersTeamLabel.Location = new System.Drawing.Point(425, 301);
+            this.PlayersTeamLabel.Name = "PlayersTeamLabel";
+            this.PlayersTeamLabel.Size = new System.Drawing.Size(52, 17);
+            this.PlayersTeamLabel.TabIndex = 18;
+            this.PlayersTeamLabel.Text = "Team:";
+            // 
+            // PlayersMatchesAmountLabel
+            // 
+            this.PlayersMatchesAmountLabel.AutoSize = true;
+            this.PlayersMatchesAmountLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayersMatchesAmountLabel.Font = new System.Drawing.Font("Showcard Gothic", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayersMatchesAmountLabel.Location = new System.Drawing.Point(26, 353);
+            this.PlayersMatchesAmountLabel.Name = "PlayersMatchesAmountLabel";
+            this.PlayersMatchesAmountLabel.Size = new System.Drawing.Size(72, 30);
+            this.PlayersMatchesAmountLabel.TabIndex = 19;
+            this.PlayersMatchesAmountLabel.Text = "Matches \r\namount:";
+            // 
+            // PositionOnFieldTextBox
+            // 
+            this.PositionOnFieldTextBox.Location = new System.Drawing.Point(513, 363);
+            this.PositionOnFieldTextBox.Name = "PositionOnFieldTextBox";
+            this.PositionOnFieldTextBox.Size = new System.Drawing.Size(146, 20);
+            this.PositionOnFieldTextBox.TabIndex = 20;
+            // 
+            // PlayersPositionLabel
+            // 
+            this.PlayersPositionLabel.AutoSize = true;
+            this.PlayersPositionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayersPositionLabel.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayersPositionLabel.Location = new System.Drawing.Point(394, 363);
+            this.PlayersPositionLabel.Name = "PlayersPositionLabel";
+            this.PlayersPositionLabel.Size = new System.Drawing.Size(85, 17);
+            this.PlayersPositionLabel.TabIndex = 21;
+            this.PlayersPositionLabel.Text = "Position:\r\n";
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.BackColor = System.Drawing.Color.Khaki;
+            this.SubmitButton.Font = new System.Drawing.Font("Showcard Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SubmitButton.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.SubmitButton.Location = new System.Drawing.Point(309, 408);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(170, 30);
+            this.SubmitButton.TabIndex = 22;
+            this.SubmitButton.Text = "Submit!";
+            this.SubmitButton.UseVisualStyleBackColor = false;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // CreateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.PlayersPositionLabel);
+            this.Controls.Add(this.PositionOnFieldTextBox);
+            this.Controls.Add(this.PlayersMatchesAmountLabel);
+            this.Controls.Add(this.PlayersTeamLabel);
+            this.Controls.Add(this.PlayersNumberLabel);
+            this.Controls.Add(this.PlayersGoalsLabel);
+            this.Controls.Add(this.PlayersAssistsLabel);
+            this.Controls.Add(this.PlayersCountryLabel);
+            this.Controls.Add(this.PlayersAgeLabel);
+            this.Controls.Add(this.FirstNameLabel);
+            this.Controls.Add(this.LastNameLabel);
+            this.Controls.Add(this.MainLabel);
+            this.Controls.Add(this.MatchesAmountTextbox);
+            this.Controls.Add(this.NumberTextbox);
+            this.Controls.Add(this.TeamTextbox);
+            this.Controls.Add(this.GoalsTextbox);
+            this.Controls.Add(this.AssistsTextbox);
+            this.Controls.Add(this.AgeTextbox);
+            this.Controls.Add(this.CountryTextbox);
+            this.Controls.Add(this.LastNameTextbox);
+            this.Controls.Add(this.FirstNameTextbox);
+            this.Name = "CreateForm";
+            this.Text = "CreateForm";
+            this.Load += new System.EventHandler(this.CreateForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstNameTextbox;
+        private System.Windows.Forms.TextBox LastNameTextbox;
+        private System.Windows.Forms.TextBox CountryTextbox;
+        private System.Windows.Forms.TextBox AgeTextbox;
+        private System.Windows.Forms.TextBox AssistsTextbox;
+        private System.Windows.Forms.TextBox GoalsTextbox;
+        private System.Windows.Forms.TextBox TeamTextbox;
+        private System.Windows.Forms.TextBox NumberTextbox;
+        private System.Windows.Forms.TextBox MatchesAmountTextbox;
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.Label LastNameLabel;
+        private System.Windows.Forms.Label FirstNameLabel;
+        private System.Windows.Forms.Label PlayersAgeLabel;
+        private System.Windows.Forms.Label PlayersCountryLabel;
+        private System.Windows.Forms.Label PlayersAssistsLabel;
+        private System.Windows.Forms.Label PlayersGoalsLabel;
+        private System.Windows.Forms.Label PlayersNumberLabel;
+        private System.Windows.Forms.Label PlayersTeamLabel;
+        private System.Windows.Forms.Label PlayersMatchesAmountLabel;
+        private System.Windows.Forms.TextBox PositionOnFieldTextBox;
+        private System.Windows.Forms.Label PlayersPositionLabel;
+        private System.Windows.Forms.Button SubmitButton;
+    }
+}
